becf47b827c382d0b55b923fca0162a2372ead46
Vikram Kumar <vikrakum@vikrakum.remote.csb>
2023-03-07 11:26:34 +0530

    commiting all changes b4 starting sync process
    Signed-off-by: Vikram Kumar <vikrakum@vikrakum.remote.csb>

--- repoSync.sh
+++ repoSync.sh
@@ -18,21 +18,20 @@ repoSyncing() {
 
     # Cheking if working tree clean!
     local branch=$(git rev-parse --abbrev-ref HEAD)
-    echo "Current working branch = $branch"
-    echo "Cheking if there is nothing to commit"
+    echo "--Current working branch = $branch"
+    echo "--Cheking if there is nothing to commit"
     unstaged_num_files=$(git status --porcelain  | { egrep '^\s?[?]' || true; } | wc -l)
     staged_num_files=$(git status --porcelain  | { egrep '^\s?[MADRC]' || true; } | wc -l)
     if ((unstaged_num_files>0 || staged_num_files>0)); then
         if ((staged_num_files>0 && unstaged_num_files > 0)); then
-            echo "$unstaged_num_files Unstaged and $staged_num_files Staged files found, Cleaning working tree to proceed!"
+            echo "--$unstaged_num_files Unstaged and $staged_num_files Staged files found, Cleaning working tree to proceed!"
         elif ((unstaged_num_files == 0 && staged_num_files>0)); then
-            echo "$staged_num_files Staged files found, Cleaning working tree to proceed!"
+            echo "--$staged_num_files Staged files found, Cleaning working tree to proceed!"
         elif ((unstaged_num_files > 0 && staged_num_files==0)); then
-            echo "$unstaged_num_files Unstaged files found, Cleaning working tree to proceed!"
+            echo "--$unstaged_num_files Unstaged files found, Cleaning working tree to proceed!"
         fi
         git add .
-        echo "Enter commit message"
-        echo "Commiting and pushing all changes made, with DCO sign"
+        echo "--Commiting and pushing all changes made, with DCO sign"
         git commit -s -m "commiting all changes b4 starting sync process"
         git push -u origin ${branch}
     else
@@ -41,18 +40,17 @@ repoSyncing() {
 
     echo "----switching to master branch----"
     git checkout master
-    echo "Branches List"
+    echo "--Branches List"
     git branch
-    echo "Enter sync branch name"
     echo "----switching to sync branch----"
     git checkout ${syncBranch}
-    echo "fetching + merging changes from ORIGIN master"
+    echo "--fetching + merging changes from ORIGIN master"
     git fetch origin master
     git merge origin/master
-    echo "fetching + merging changes from UPSTREAM master"
+    echo "--fetching + merging changes from UPSTREAM master"
     git fetch upstream ${target_upstream_branch}
     git merge --strategy-option=ours upstream/${target_upstream_branch}
-    echo "Logging recent details for you."
+    echo "--Logging recent details for you."
     git log --oneline --max-count=10
     git push -u origin ${syncBranch}
     
